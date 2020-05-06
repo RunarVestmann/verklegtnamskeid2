0b7ed66dbd0e0abdb9664c280cc4a87100182bd3
RunarVestmann <runarvestmann@gmail.com>
2020-05-06 14:34:16 +0000

    Added some temporary code for the search

--- captain_console/product/views.py
+++ captain_console/product/views.py
@@ -5,40 +5,62 @@ from .forms import ProductForm
 from .models import Product, ProductImage, Type, System
 
 def index(request):
-    if 'search' in request.GET and 'render' not in request.GET:
-        search = request.GET['search'].split(' ')
+    if 'search' in request.GET:
+        search = request.GET['search'].strip().split()
 
-        search_results = QuerySet()
+        if search:
+            search_results = Product.objects.filter(name__icontains=search[0]) \
+                           | Product.objects.filter(system__name__icontains=search[0]) \
+                           | Product.objects.filter(system__manufacturer__name__icontains=search[0]) \
+                           | Product.objects.filter(system__abbreviation__icontains=search[0]) \
+                           | Product.objects.filter(type__name__icontains=search[0])
 
-        for word in search:
-            search_results = search_results \
-                           | Product.objects.filter(name__icontains=word) \
-                           | Product.objects.filter(system__name__icontains=word) \
-                           | Product.objects.filter(system__manufacturer__name__icontains=word) \
-                           | Product.objects.filter(system__abbreviation__icontains=word) \
-                           | Product.objects.filter(type__name__icontains=word)
+            for word in search[1:]:
+                search_results = search_results \
+                               | Product.objects.filter(name__icontains=word) \
+                               | Product.objects.filter(system__name__icontains=word) \
+                               | Product.objects.filter(system__manufacturer__name__icontains=word) \
+                               | Product.objects.filter(system__abbreviation__icontains=word) \
+                               | Product.objects.filter(type__name__icontains=word)
 
-        products = [{
-            'id': p.id,
-            'name': p.name,
-            'quantity': p.quantity,
-            'description': p.description,
-            'price': p.price,
-            'system': {
+                products = [{
+                    'id': p.id,
+                    'name': p.name,
+                    'quantity': p.quantity,
+                    'description': p.description,
+                    'price': p.price,
+                    'system': {
                         'manufacturer': p.system.manufacturer.name,
                         'abbreviation': p.system.abbreviation,
                         'name': p.system.name
-                        },
-            'release_date': p.release_date,
-            'shop_arrival_date': p.shop_arrival_date,
-            'type': p.type.name,
-            'first_image': p.productimage_set.first().image.url
-        } for p in search_results]
+                    },
+                    'release_date': p.release_date,
+                    'shop_arrival_date': p.shop_arrival_date,
+                    'type': p.type.name,
+                    'first_image': p.productimage_set.first().image.url
+                } for p in search_results]
 
-        return JsonResponse({'data': products})
+                return JsonResponse({'data': products})
 
-    elif 'search' in request.GET and 'render' in request.GET:
-        print(100)
+        else:
+            products = [{
+                'id': p.id,
+                'name': p.name,
+                'quantity': p.quantity,
+                'description': p.description,
+                'price': p.price,
+                'system': {
+                    'manufacturer': p.system.manufacturer.name,
+                    'abbreviation': p.system.abbreviation,
+                    'name': p.system.name
+                },
+                'release_date': p.release_date,
+                'shop_arrival_date': p.shop_arrival_date,
+                'type': p.type.name,
+                'first_image': p.productimage_set.first().image.url
+            } for p in Product.objects.all()]
+
+            return JsonResponse({'data': products})
 
     return render(request, 'product/products.html', {
         'products': Product.objects.all(),
--- captain_console/static/js/products.js
+++ captain_console/static/js/products.js
@@ -6,9 +6,16 @@ $(document).ready(function(){
             url: '/products?search=' + searchText,
             type: 'GET',
             success: function(response){
-                const newHTML = response.data.map(d => {
-                    return `<div>
-
+                const newHTML = response.data.map(p => {
+                    return `<div class="card" style="width: 18rem; background-color: #38b8c3; margin:1rem;">
+                                <img class="card-img-top" src="${ p.first_image }" alt="Product image" style="height: 60%">
+                                <div class="card-body">
+                                    <h5 class="card-title">${ p.name }</h5>
+                                    <p class="card-text">${ p.system.manufacturer } / ${ p.system.abbreviation } / ${ p.type }</p>
+                                    <p>Verð: <strong>${ p.price }</strong> ISK</p>
+                                    <a href="/products/${p.id}" class="btn btn-primary">Sjá nánar</a>
+                                    <a href="#" class="btn btn-success">Setja í körfu</a>
+                                </div>
                             </div>`
                 });
                 $('#products').html(newHTML.join(''));
