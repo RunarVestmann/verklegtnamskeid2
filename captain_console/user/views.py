fdd38278acb151af7dd70810cff66349691c17ed
Guðmundur Sverrisson <gudmundursvr@gmail.com>
2020-05-06 10:08:54 +0000

    user profil and links in header

--- captain_console/user/views.py
+++ captain_console/user/views.py
@@ -4,9 +4,25 @@ from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
 def index(request):
     return render(request, 'user/user.html')
+
+# sometest to put singup and login in tabs
+# result so far login crash everyting problem with reload sign up if not success
+
+# def index(request):
+#     form = SignUpForm(request.POST)
+#     if form.is_valid():
+#         form.save()
+#         username = form.cleaned_data.get('username')
+#         password = form.cleaned_data.get('password1')
+#         user = authenticate(username=username, password=password)
+#         login(request, user)
+#         return redirect('user')
+#     return render(request, 'user/user.html', {'form': form})
+
 
 
 def registration(request):
