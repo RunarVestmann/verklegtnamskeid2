47fcfad662c8b036d0cf7a42e615208965b3c67c
RunarVestmann <runarvestmann@gmail.com>
2020-05-09 02:56:06 +0000

    The local storage is now functional, check out /cart to manage your cart

--- captain_console/static/js/cart.js
+++ captain_console/static/js/cart.js
@@ -1,3 +1,5 @@
+let cartProducts = [];
+
 const cart = {
     key: 'cart',
     products: [],
@@ -6,15 +8,13 @@ const cart = {
 
         if(storedProducts)
             cart.products = JSON.parse(storedProducts);
-        else{
-            cart.products = [];
-            cart.sync();
-        }
+        else
+            cart.save();
     },
 
     // Sync the local storage with what's in the cart object
-    async sync(){
-        await localStorage.setItem(cart.key, JSON.stringify(cart.products));
+    save(){
+        localStorage.setItem(cart.key, JSON.stringify(cart.products));
     },
 
     find(id){
@@ -22,20 +22,29 @@ const cart = {
     },
 
     add(id, amount=1){
-        const product = cart.find(id);
-        if(product){
-            product.quantity += amount;
-            cart.sync();
+        const productInStorage = cart.find(id);
+        if(productInStorage){
+            productInStorage.quantity += amount;
+            cart.save();
         }
         else{
             $.ajax({
                 url: `/products/${id}/json`,
                 type: 'GET',
                 success: function(response){
-                    const prod = response.data;
-                    if(prod){
-                        prod.quantity = 1;
-                        cart.products.push(prod);
+                    const productInStorage = cart.find(id);
+                    if(productInStorage){
+                        productInStorage.quantity = Number(productInStorage.quantity);
+                        productInStorage.quantity += amount;
+                        cart.save();
+                        return;
+                    }
+
+                    const productFromServer = response.data;
+                    if(productFromServer){
+                        productFromServer.quantity = 1;
+                        cart.products.push(productFromServer);
+                        cart.save();
                     }
                     else{
                         // TODO: Show a message that says that the product id was not found
@@ -50,95 +59,96 @@ const cart = {
         }
     },
 
-    remove(id, amount=1){
+    changeQuantity(id, quantity){
         const product = cart.find(id);
-
         if(product){
-            product.quantity -= amount;
-
-            // Remove the item all together if it's quantity is at or below zero
-            if(product.quantity <= 0){
-                const index = cart.products.indexOf(product);
-                if(index > -1)
-                    cart.products.splice(index, 1);
-            }
-            cart.sync();
+            product.quantity = quantity;
+            cart.save();
         }
     },
 
-    delete(id){
+    remove(id, amount=1){
+        const productInStorage = cart.find(id);
+
+        if(productInStorage){
+            productInStorage.quantity -= amount;
+
+            // Remove the item all together if it's quantity is at or below zero
+            if(productInStorage.quantity <= 0)
+                removeItemFromArray(cart.products, productInStorage);
+            cart.save();
+        }
+    },
+
+    removeAll(id){
         const product = cart.find(id);
         if(product){
-            const index = cart.products.indexOf(product);
-            if(index > -1){
-                cart.products.splice(index, 1);
-                cart.sync();
-            }
+            removeItemFromArray(cart.products, product);
+            cart.save();
         }
     },
 
     clear(){
         cart.products = [];
-        cart.sync();
+        cart.save();
     }
 }
 
-$(document).ready(() =>{
+function removeItemFromArray(array, item){
+    const index = array.indexOf(item);
+        if(index > -1){}
+            array.splice(index, 1);
+}
+
+$(document).ready(function(){
     cart.init();
-    if(window.location.pathname === '/cart/'){
-        const newHTML = cart.products.map(p => {
-            return `<div class="product-cart-view">
-                        <div class="product-cart-image">
-                          <img src="${p.first_image}">
-                        </div>
-                        <div class="product-details">
-                          <div class="product-title">${p.name}</div>
-                        </div>
-                        <div class="product-price">${p.price.toLocaleString('is').replace(',', '.')}</div>
-                        <div class="product-quantity">
-                          <input type="number" value="${p.quantity}" min="1" onchange="updateQuantity(this , ${p.id})">
-                        </div>
-                        <div class="product-removal" >
-                          <button class="remove-product" onclick="removeItem(this, ${p.id});">
-                            Remove
-                          </button>
-                        </div>
-                        <div class="product-line-price">${p.price * p.quantity}</div>
-                        </div>`;
-        });
-        $('#products').html(newHTML.join(''));
-    }
+    if(window.location.pathname === '/cart/')
+        renderProductsInCart();
 });
 
-function addToCart(id, ){
-    cart.add(id);
-    console.log(cart);
+function renderProductsInCart(){
+    const newHTML = cart.products.map(p => {
+        return `<div class="product-cart-view">
+                    <div class="product-cart-image">
+                      <img src="${p.first_image}">
+                    </div>
+                    <div class="product-details">
+                      <div class="product-title">${p.name}</div>
+                    </div>
+                    <div class="product-price">${p.price.toLocaleString('is').replace(',', '.')}</div>
+                    <div class="product-quantity">
+                      <input type="number" value="${p.quantity}" min="1" onchange="updateQuantity(this , ${p.id})">
+                    </div>
+                    <div class="product-removal" >
+                      <button class="remove-product" onclick="removeItem(this, ${p.id});">
+                        Remove
+                      </button>
+                    </div>
+                    <div class="product-line-price">${(p.price * p.quantity).toLocaleString('is').replace(',', '.')}</div>
+                    </div>`;
+    });
+    $('#products').html(newHTML.join(''));
 }
 
-function removeFromCart(){
-    cart.remove(id);
-}
-
-function clearCart(){
-    cart.clear();
+function addToCart(productID){
+    cart.add(productID);
 }
 
 // Credit goes to https://bootsnipp.com/snippets/qrOrg
-
 /* Set rates + misc */
-let shippingRate = 15.00;
+let shippingRate = 1500;
 let fadeTime = 300;
 
 /* Remove item from cart */
 function removeItem(removeButton, id)
 {
-  /* Remove row from DOM and recalc cart total */
-  let productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
-      productRow.remove();
-      recalculateCart();
-      cart.delete(id);
-  });
+    /* Remove row from DOM and recalc cart total */
+    let productRow = $(removeButton).parent().parent();
+    productRow.slideUp(fadeTime, function() {
+        productRow.remove();
+        recalculateCart();
+        cart.removeAll(id);
+    });
 }
 
 /* Recalculate cart */
@@ -148,17 +158,17 @@ function recalculateCart()
 
   /* Sum up row totals */
   $('.product').each(function () {
-    subtotal += parseFloat($(this).children('.product-line-price').text());
+    subtotal += $(this).children('.product-line-price').text();
   });
 
   /* Calculate totals */
   let shipping = (subtotal > 0 ? shippingRate : 0);
-  let total = subtotal + shipping;
+  let total = subtotal;// + shipping;
 
   /* Update totals display */
   $('.totals-value').fadeOut(fadeTime, function() {
     $('#cart-subtotal').html(subtotal);
-    $('#cart-shipping').html(shipping);
+    //$('#cart-shipping').html(shipping);
     $('#cart-total').html(total);
     if(total == 0){
       $('.checkout').fadeOut(fadeTime);
@@ -172,26 +182,23 @@ function recalculateCart()
 /* Update quantity */
 function updateQuantity(quantityInput, id)
 {
+  let quantity = $(quantityInput).val();
+  if(quantity <= 0)
+      return removeItem(quantityInput, id);
+
   /* Calculate line price */
   let productRow = $(quantityInput).parent().parent();
-  let price = parseFloat(productRow.children('.product-price').text());
-  let quantity = $(quantityInput).val();
+  let price = Number(productRow.children('.product-price').text().replace('.', ''));
   let linePrice = price * quantity;
 
-  const product = cart.find(id);
-  if(product) {
-      product.quantity = quantity;
-      cart.sync();
-  }
+  cart.changeQuantity(id, quantity);
 
   /* Update line price display and recalc cart totals */
   productRow.children('.product-line-price').each(function () {
     $(this).fadeOut(fadeTime, function() {
-      $(this).text(linePrice.toFixed(2));
+      $(this).text(linePrice.toLocaleString('is').replace(',', '.'));
       recalculateCart();
       $(this).fadeIn(fadeTime);
     });
   });
-
-
 }
